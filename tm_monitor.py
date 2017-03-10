92b5347d788519136c90fb47634503e887e77145
jgonzaga <joel.gonzaga@example.com>
2017-03-10 09:51:32 -0800

    created an outline in comments of what I want this program to do.

---
+++ tm_monitor.py
@@ -0,0 +1,14 @@
+# this will be where I make the TM monitor.
+
+# Import Relevant modules to access Mac OS.
+
+#Write a function that (Check) checks the last time Machine back.
+# Takes no Paramters. Returns the time of the last back up as a Date.
+
+#write another function. (Compare) Takes in a a date. Compares it to current System time.
+# Returns 'on time' 'late' 'very late' 'error'
+
+#Fucntion that runs on a infinute loop.
+#Runs Compare every two hours.
+#Prints out results.
+#If Very late, makes a system beep.

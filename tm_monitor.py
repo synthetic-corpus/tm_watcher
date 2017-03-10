c06d641ff0b7e907f370c276684c2d5f4dc9fb08
jgonzaga <joel.gonzaga@example.com>
2017-03-10 15:13:42 -0800

    get the string function stopped working for some reason.

--- tm_monitor.py
+++ tm_monitor.py
@@ -36,29 +36,42 @@ def terminalToDatetime(string):
 def readBackUpOutput(string):
     #Checks for the errro message using string.find
     if string.find("Unable to locate machin") != -1:
-        return "Back up not Configured"
+        return "disconnected"
     else:
-        #Runs a process to be written and eventually returns a time stamp variable.
-        return "This is the last time it backed up"
+        #Compares time. Ensures not longer than 90 minutes.
+        last_good_backup = terminalToDatetime(string)
+        current_time = datetime.datetime.now()
+        gap = current_time - last_good_backup
+        ninety_minutes = datetime.timedelta(hours = 1, minutes =30)
+        if gap < ninety_minutes:
+            return "working"
+        else:
+            return "overdue"
 
-def getBackupStatus():
+def getBackupString():
     #Returns an immutable tuple. Second thing in tuple is what I need as a string.
     rawout = subprocess.Popen(["tmutil","latestbackup"], stdout=PIPE, stderr=PIPE)
     #String_out is what you would get if you enter 'tmutil latestbackup' in terminal. Is a string.
     string_out = rawout.communicate()[1]
     print string_out
-    status = readBackUpOutput(string_out)
-    return status
+    print rawout.communicate()
+    return string_out
 
-print getBackupStatus()
+def networkOutput():
+    terminal_output = getBackupString()
+    #print terminal_output
+    machine_ID = 'machine ID' #Use mac OS module to get machine serial
+    machine_name = 'machine name' #Get Mac OS module to get machine name
+    if readBackUpOutput(terminal_output) == "disconnected":
+        #Send network 'red signal with machine info.
+        print "Back up for ",machine_name," is offline."
+    elif readBackUpOutput(terminal_output) == "overdue":
+        #Send network 'yellow signal' with machine info.
+        print "Back up for ",machine_name," is overdue."
+    elif readBackUpOutput(terminal_output) == "working":
+        #Send network Green signal with machine info
+        print "Back up for ",machine_name," is working."
+    else:
+        print "something is wrong with the code. What went wrong?"
 
-# Write a fucntion that takes a String as Parameter. Reads it. Returns "no Back up Present" or "Date of last back up"
-
-
-#write another function. (Compare) Takes in a a date. Compares it to current System time.
-# Returns 'on time' 'late' 'very late' 'error'
-
-#Fucntion that runs on a infinute loop.
-#Runs Compare every two hours.
-#Prints out results.
-#If Very late, makes a system beep.
+print getBackupString()

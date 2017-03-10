b3fd24c338b3d0c47c71ec4c1b8a6930c15076a6
jgonzaga <joel.gonzaga@example.com>
2017-03-10 11:16:05 -0800

    readBackUpStatus() function now checks for one error as expected.

--- tm_monitor.py
+++ tm_monitor.py
@@ -7,14 +7,27 @@ import subprocess
 from subprocess import PIPE
 #Write a function that (Check) checks the last time Machine back.
 # Takes no Paramters. Returns the time of the last back up as a Date.
-rawout = subprocess.Popen(["tmutil","latestbackup"], stdout=PIPE, stderr=PIPE)
-string = rawout.communicate()[1]
-print string[2:]
-'''def check_last_backup():
-    #time_stamp = subprocess.check_output("ls -l")
-    print subprocess.check_output("tmutil latestbackup")
+def readBackUpStatus(string):
+    #Checks for the errro message using string.find
+    if string.find("Unable to locate machin") != -1:
+        return "Back up not Configured"
+    else:
+        return "This is the last time it backed up"
 
-check_last_backup()'''
+def getBackupStatus():
+    #Returns an immutable tuple. Second thing in tuple is what I need as a string.
+    rawout = subprocess.Popen(["tmutil","latestbackup"], stdout=PIPE, stderr=PIPE)
+    #String_out is what you would get if you enter 'tmutil latestbackup' in terminal. Is a string.
+    string_out = rawout.communicate()[1]
+    print string_out
+    status = readBackUpStatus(string_out)
+    return status
+
+print getBackupStatus()
+
+# Write a fucntion that takes a String as Parameter. Reads it. Returns "no Back up Present" or "Date of last back up"
+
+
 #write another function. (Compare) Takes in a a date. Compares it to current System time.
 # Returns 'on time' 'late' 'very late' 'error'
 

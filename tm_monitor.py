7fc086ada44a598162639130433393bf28885b19
jgonzaga <joel.gonzaga@example.com>
2017-03-10 10:52:06 -0800

    Got a command to return a string. Weird as hell, but it's progress.

--- tm_monitor.py
+++ tm_monitor.py
@@ -1,10 +1,20 @@
 # this will be where I make the TM monitor.
 
-# Import Relevant modules to access Mac OS.
 
+# Import Relevant modules to access Mac OS.
+import os
+import subprocess
+from subprocess import PIPE
 #Write a function that (Check) checks the last time Machine back.
 # Takes no Paramters. Returns the time of the last back up as a Date.
+rawout = subprocess.Popen(["tmutil","latestbackup"], stdout=PIPE, stderr=PIPE)
+string = rawout.communicate()[1]
+print string[2:]
+'''def check_last_backup():
+    #time_stamp = subprocess.check_output("ls -l")
+    print subprocess.check_output("tmutil latestbackup")
 
+check_last_backup()'''
 #write another function. (Compare) Takes in a a date. Compares it to current System time.
 # Returns 'on time' 'late' 'very late' 'error'
 

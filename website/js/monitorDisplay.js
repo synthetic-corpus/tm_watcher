955b16c0470a2eff5b846cc07c612062eb329edb
jgonzaga <joel.gonzaga@example.com>
2017-03-16 17:42:32 -0700

    added basic Javascript HTML templates.

--- website/js/monitorDisplay.js
+++ website/js/monitorDisplay.js
@@ -3,9 +3,12 @@ This will be the Java Script File
 */
 var dataList = {};
 
-var HTMLrow = "<tr><td>%status%</td><td>%nomen%</td><td>%lastcheckin%</td><td>%serial%</td></tr>"
+// Helper HTML strings. To be used when appending to the table.
+var HTMLrow = "<tr><td>%status%</td><td>%nomen%</td><td>%lastcheckin%</td><td>%serial%</td></tr>";
+var statusGreen = "<div class='green-dot'>Working</div>";
+var statusRed = "<div class='red-dot'>Disconnected</div>";
+var statusYellow = "<div class='yellow-dot'>No recent back up</div>";
+var statusBlack ="<div class='black-dot'>Overdue for Check in>/div>";
 
-$("#putstuffhere").append(HTMLrow);
-$("#putstuffhere").append(HTMLrow.replace("%status%","something else"));
-$("#putstuffhere").append(HTMLrow);
-$("#putstuffhere").append(HTMLrow);
+// Append statement. To be used much later.
+// $("#putstuffhere").append(HTMLrow);

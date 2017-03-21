087202453ef005c9b0d4b6b67214ca5e30a82d99
jgonzaga <joel.gonzaga@example.com>
2017-03-21 15:54:37 -0700

    Can now pull data from python listener code, to a javascript array, which then builds a display dynamically.

--- website/js/monitorDisplay.js
+++ website/js/monitorDisplay.js
@@ -2,8 +2,6 @@
 This will be the Java Script File
 */
 
-console.log(statusArray);
-
 // Helper HTML strings. To be used when appending to the table.
 var HTMLrow = "<tr><td>%status%</td><td>%nomen%</td><td>%lastcheckin%</td><td>%serial%</td></tr>";
 var statusGreen = "<div class='green-dot'>Working</div>";
@@ -11,6 +9,17 @@ var statusRed = "<div class='red-dot'>Disconnected</div>";
 var statusYellow = "<div class='yellow-dot'>No recent back up</div>";
 var statusBlack ="<div class='black-dot'>Overdue for Check in>/div>";
 
+function makeTable(statusArray){
+  statusArray.forEach(function(entry){
+    status = entry.status;
+    name = entry.name;
+    timestring = entry.timestring;
+    serial = entry.serial;
+    formatted_row = HTMLrow.replace("%status%",status).replace("%nomen%",name).replace("%serial%",serial).replace("%lastcheckin%",timestring);
+    $("#putstuffhere").append(formatted_row);
+  })
+};
+makeTable(statusArray);
 // Append statement. To be used much later.
-$("#putstuffhere").append(HTMLrow);
+
 // loadlist();

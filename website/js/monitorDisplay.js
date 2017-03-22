75c9509de5f9002581b3b4565e8a8a53d94779e3
jgonzaga <joel.gonzaga@example.com>
2017-03-21 17:34:28 -0700

    added a condition that checks for how long since last check in.

--- website/js/monitorDisplay.js
+++ website/js/monitorDisplay.js
@@ -7,11 +7,11 @@ var HTMLrow = "<tr><td>%status%</td><td>%nomen%</td><td>%lastcheckin%</td><td>%s
 var statusGreen = "<div class='green-dot'>Working</div>";
 var statusRed = "<div class='red-dot'>Disconnected</div>";
 var statusYellow = "<div class='yellow-dot'>No recent back up</div>";
-var statusBlack ="<div class='black-dot'>Overdue for Check in>/div>";
+var statusBlack ="<div class='black-dot'>Overdue for Check in</div>";
 
 function makeTable(statusArray){
   statusArray.forEach(function(entry){
-    status = checkStatus(entry.status);
+    status = checkStatus(entry);
     name = entry.name;
     timestring = entry.timestring;
     serial = entry.serial;
@@ -20,14 +20,30 @@ function makeTable(statusArray){
   })
 };
 
-function checkStatus(status){
-  if (status === "working"){
+function checkTime(entry){
+  var inputTime = new Date(entry.timestring);
+  var currentTime = new Date(Date.now());
+  var timeDelta = (currentTime - inputTime)/1000/60; //Converted into minutes.
+  var too_long = 12 * 60 // Twelve hours.
+  if (timeDelta >= too_long){
+    return 'no check ins';
+  }
+  else{
+    return 'recent';
+  }
+}
+
+function checkStatus(entry){
+  if (checkTime(entry) === 'no check ins'){
+    return statusBlack;
+  }
+  else if (entry.status === "working"){
     return statusGreen;
   }
-  else if (status === "disconnected"){
+  else if (entry.status === "disconnected"){
     return statusRed;
   }
-  else if (status === "overdue") {
+  else if (entry.status === "overdue") {
     return statusYellow;
   };
 }
